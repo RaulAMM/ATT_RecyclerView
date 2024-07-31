185271239a09badd8ea72d2773693e93ce1ab99f
RaulAMM <raul.martins@aluno.ifsp.edu.br>
2024-07-31 09:18:46 -0300

    Ainda falta o Done

---
+++ app/src/main/java/nr/edu/ifsp/dmo/sitesfavoritos/model/Site.kt
@@ -0,0 +1,5 @@
+package nr.edu.ifsp.dmo.sitesfavoritos.model
+
+class Site(var apelido: String, var url: String) {
+    var favorito: Boolean = false
+}
---
+++ app/src/main/java/nr/edu/ifsp/dmo/sitesfavoritos/view/listeners/SiteItemClickListener.kt
@@ -0,0 +1,7 @@
+package nr.edu.ifsp.dmo.sitesfavoritos.view.listeners
+
+interface SiteItemClickListener {
+    fun clickSiteItem(position: Int)
+
+    fun clickHeartSiteItem(position: Int)
+}
